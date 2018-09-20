36ad3f6a72a8fd73c2eb2ab660fa3f3fd6840fd9
wangwendi <wangwendi@51xianqu.net>
2018-09-20 17:10:08 +0800

    初始化

---
+++ src/main/java/com/asyncmd/model/AsynCmdDO.java
@@ -0,0 +1,13 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2018 All Rights Reserved.
+ */
+package com.asyncmd.model;
+
+/**
+ *
+ * @author wangwendi
+ * @version $Id: AsynCmdDO.java, v 0.1 2018年09月20日 下午5:09 wangwendi Exp $
+ */
+public class AsynCmdDO {
+}
